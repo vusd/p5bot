8325699b46333e93211816a65fe492111f9f07f5
Tom White <tom@sixdozen.com>
2016-08-30 00:18:19 +1200

    First p5grab example

---
+++ example/sketch.js
@@ -0,0 +1,49 @@
+var rndSeed;
+
+function setup () {
+  createCanvas(440, 220);
+  rndSeed = random(1024);
+}
+
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+  else if (key == '@') {
+    saveBlocksImages(true);
+  }
+}
+
+function draw() {
+  background(204);
+  // randomSeed(0);
+  resetFocusedRandom(rndSeed);
+  var num_owls = 0;
+  for (var i = 35; i < width - 40; i += 40) {
+    var gray = int(focusedRandom(0, 102, 3))
+    var scalar = focusedRandom(0.15, 0.75, 2);
+    // var gray = int(random(0, 102));
+    // var scalar = random(0.15, 0.75);
+    owl(i, 160, gray, scalar);
+    num_owls = num_owls + 1;
+  }
+}
+
+function owl(x, y, g, s) {
+  push();
+  translate(x, y);
+  scale(s);  // Set the createCanvas
+  stroke(g); // Set the gray value
+  strokeWeight(70);
+  line(0, -35, 0, -65); // Body
+  noStroke();
+  fill(255-g);
+  ellipse(-17.5, -65, 35, 35); // Left eye dome
+  ellipse(17.5, -65, 35, 35);  // Right eye dome
+  arc(0, -65, 70, 70, 0, PI);  // Chin
+  fill(g);
+  ellipse(-14, -65, 8, 8);  // Left eye
+  ellipse(14, -65, 8, 8);   // Right eye
+  quad(0, -58, 4, -51, 0, -44, -4, -51); // Beak
+  pop();
+}
---
+++ test.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from PIL import Image
+from io import BytesIO
+
+driver = webdriver.PhantomJS() # or add to your PATH
+driver.set_window_size(440, 220) # optional
+driver.get('example/index.html')
+img = driver.get_screenshot_as_png()
+img = Image.open(BytesIO(img))
+cropped = img.crop((0,0,440,220))
+cropped.save('image.png')
+# driver.save_screenshot('screen.png') # save a screenshot to disk
