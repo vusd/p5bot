0555111a4f5f03a3191e663fe74afc4961446bfa
Tom White <tom@sixdozen.com>
2016-10-20 01:17:19 +1300

    do_once.sh: added a retry on first timeout

--- do_once.sh
+++ do_once.sh
@@ -5,6 +5,7 @@
 # here is an example crontab line
 # 5 * * * * cd /develop/bots/p5bot && ./do_once.sh
 
+# phantomjs needs to be in path
 PATH=$PATH:/usr/local/bin
 
 echo "--> RUNNING "`date` >> run_log.txt
@@ -12,3 +13,11 @@ echo "--> RUNNING "`date` >> run_log.txt
 /usr/bin/unbuffer /usr/local/anaconda2/envs/p5bot/bin/python \
 	tweet.py -t 60 -s current --swaplist choice.json \
     >> run_log.txt 2>&1
+
+# check exit code, rerun if timeout
+rc=$?; if [[ $rc != 0 ]]; then
+  echo "RERUNNING FROM TIMEOUT" >> run_log.txt
+  /usr/bin/unbuffer /usr/local/anaconda2/envs/p5bot/bin/python \
+	tweet.py -t 60 -s current \
+    >> run_log.txt 2>&1
+fi
